8523d80bcc2df394d78c8289ab4b8f524ee137aa
Annciatar Ndumi <annciatar.kavita@student.moringaschool.com>
2022-08-23 13:52:53 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    workList(e.target.newTask.value)
+  })
 });
+function workList(eachItem) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button') 
+btn.addEventListener('click', forDelete)
+btn.textContent = 'x'
+p.textContent = `${eachItem}`
+p.appendChild(btn)
+console.log(p)
+document.querySelector('#list').appendChild(p)
+
+}
+function forDelete(e){
+  e.target.parentNode.remove()
+}
