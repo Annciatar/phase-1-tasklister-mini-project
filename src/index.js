0e155bdeb968c264204b61d24ee5f6375b29ceb6
Annciatar Ndumi <annciatar.kavita@student.moringaschool.com>
2022-08-24 10:56:20 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault()
-    workList(e.target.newTask.value)
+    workList(e.target.lastElementChild.previousElementSibling.value)
   })
 });
-function workList(eachItem) {
+ function workList(eachItem) {
   let p = document.createElement('p')
   let btn = document.createElement('button') 
-btn.addEventListener('click', forDelete)
-btn.textContent = 'x'
-p.textContent = `${eachItem}`
-p.appendChild(btn)
-console.log(p)
+ btn.addEventListener('click', forDelete)
+ btn.textContent = 'x'
+ p.textContent = `${eachItem}`
+ p.appendChild(btn)
+ console.log(p)
 document.querySelector('#list').appendChild(p)
 
-}
-function forDelete(e){
+ }
+ function forDelete(e){
   e.target.parentNode.remove()
-}
+ }
